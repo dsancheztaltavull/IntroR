da90ad6924139298cbece820fbea0fae481f3a11
dsancheztaltavull <46782058+dsancheztaltavull@users.noreply.github.com>
2020-10-19 23:50:20 +0200

    Add files via upload

---
+++ Basic R concepts/Practice1.R
@@ -0,0 +1,73 @@
+#Sum 16 plus 16
+
+#Multiply 39 times 2
+
+
+#Substract 5 to 39
+
+
+#Divide 39 by 3
+
+
+#How much is 8 square?
+
+
+
+#assign the numeric value 9 to the variable queen
+queen <- 
+#assign the numeric value 3 to the variable bishop
+
+#assign the numeric value 1 to the variable pawn
+
+
+
+
+
+#check the value of the variables queen, bishop and pawn
+
+
+#assign the character "checkmate" to the variable win
+
+
+#check the value of win
+
+
+#Is 3 smaller than 4?
+
+
+#Is 7 bigger than 11?
+
+
+#Is it worth sacrifying a bishop to kill a queen?
+
+
+#Would you rather have queen or a 5 pawns and 2 bishops?
+
+
+
+#Sort alphabetically the words "rook", "king" and "knight" 
+
+#run the following two lines
+
+time <- 1:24
+temperature <- c(10,9,9,8,9,9,9.5,10,9.5,9,12,11.5,12,13,13,13.5,14,14,13,12,11,11,11,10)
+
+#Plot time in the x axis and temperature in the y axis
+
+
+
+#plot it again, connecting the points with lines
+
+
+
+#Plot the cos function
+
+
+#plot the sqrt function from 0 to 10
+
+
+
+#Read the help of the function plot
+
+
+
