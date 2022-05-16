4368deb31dea36249879d7c8ef5582eaca743330
dsancheztaltavull <46782058+dsancheztaltavull@users.noreply.github.com>
2022-05-16 13:48:41 +0200

    Update Example7.R

--- ggplot2/Example7.R
+++ ggplot2/Example7.R
@@ -26,47 +26,73 @@ summary(pc)
 names(pc)
 
 
-#Inspect the variance explained by each component
+#We can look at how much variance we explain with each component 
 percent_var <- pc$sdev^2/sum(pc$sdev^2)
-barplot(percent_var, xlab="Principle component", ylab="% of variance")
+percent_var <- percent_var*100  #From 0 to 100
+barplot(percent_var, xlab="PCs", ylab="variance (%)")
+#We observe that the first 2 principal components explain a high percentage of the variance
 
+#Alternatively, we can check it
 cum_var <- cumsum(pc$sdev^2/sum(pc$sdev^2))
+cum_var <- cum_var*100 #From 0 to 100
 barplot(cum_var, xlab="Principle component", ylab="Cumulative % of variance" )
 
-#PCA coordinates
+#We can add an horizonal with the threshold on how much variance do we need
+abline(h=80)
+#For 80% we need 3 PCs
+abline(h=60)
+#For 60% we need 2 PCs
+
+
+#the coordinates of the PCA are stored in a variable names x of the pc object
 pc$x
 head(pc$x)
 
 library(ggplot2)
 
-ggplot(aes(x=PC1, y=PC2), data=data.frame(pc$x))+
-  geom_point(size=4, alpha=0.5)+
+df <- data.frame(pc$x)
+
+ggplot(aes(x=PC1, y=PC2), data=df)+
+  geom_point(size=2, alpha=0.8)+
   theme_bw()
+
+#We can plot other PCs, for example 1 vs 3
+
+ggplot(aes(x=PC1, y=PC3), data=df)+
+  geom_point(size=2, alpha=0.8)+
+  theme_bw()
+
+
 
 
 colnames(data)
 #Add some info about the samples, in this case the day of the experiment
 Condition <- c(0,0,2,2,4,4,6,6,7.5,7.5,8,8,8.5,8.5,10,10,10.5,10.5,11,11,11.5,11.5,12,12,14,14,16,16)
 #?ggplot
-ggplot(aes(x=PC1, y=PC2, color=Group), data=data.frame(pc$x, Group=Condition))+
-  geom_point(size=4, alpha=1)+ xlab(paste0("PC1: ",100*percent_var[1],"% variance")) +
-  ylab(paste0("PC2: ",100*percent_var[2],"% variance"))+theme_bw()+
+
+df <- data.frame(pc$x, Group=Condition)
+
+ggplot(data=df, aes(x=PC1, y=PC2, color=Group))+
+  geom_point(size=2, alpha=1)+ xlab(paste0("PC1: ",round(percent_var[1],5),"% variance")) +
+  ylab(paste0("PC2: ",round(percent_var[1],5),"% variance"))+theme_bw()+
   scale_color_gradient(low="black", high="red")
 
 #Plot a meaningful gene for this dataset
 HBB <- rpm["HBB",]
 
-ggplot(aes(x=PC1, y=PC2,  color=GeneExpression), data=data.frame(pc$x, GeneExpression=HBB))+
-  geom_point(size=4, alpha=1)+ xlab(paste0("PC1: ",100*percent_var[1],"% variance")) +
-  ylab(paste0("PC2: ",100*percent_var[2],"% variance"))+theme_bw()+
+df <-  data.frame(pc$x, GeneExpression=HBB)       
+
+ggplot(data=df, aes(x=PC1, y=PC2, color=Group))+
+  geom_point(size=2, alpha=1)+ xlab(paste0("PC1: ",round(percent_var[1],5),"% variance")) +
+  ylab(paste0("PC2: ",round(percent_var[1],5),"% variance"))+theme_bw()+
   scale_color_gradient(low="black", high="red")+ggtitle("HBB")
 
 #Another one
 HBB <- rpm["SPI1",]
 
-ggplot(aes(x=PC1, y=PC2,  color=GeneExpression), data=data.frame(pc$x, GeneExpression=HBB))+
-  geom_point(size=4, alpha=1)+ xlab(paste0("PC1: ",100*percent_var[1],"% variance")) +
-  ylab(paste0("PC2: ",100*percent_var[2],"% variance"))+theme_bw()+
+ggplot(data=df, aes(x=PC1, y=PC2, color=Group))+
+  geom_point(size=2, alpha=1)+ xlab(paste0("PC1: ",round(percent_var[1],5),"% variance")) +
+  ylab(paste0("PC2: ",round(percent_var[1],5),"% variance"))+theme_bw()+
   scale_color_gradient(low="black", high="red")+ggtitle("SPI1")
 
 
@@ -116,14 +142,14 @@ ggplot(data=df,aes(x=time, y=temperature))+geom_bar(stat="identity")+
 
 
 ggplot(data=df,aes(x=time, y=temperature))+geom_bar(stat="identity")+
-  geom_line()+xlim(c(6,23))+ylim(0,30)+ggtitle("13th of October 2020")
+  geom_line()+xlim(c(6,23))+ylim(0,30)+ggtitle("18th of July 2021")
 
 
 #Change the labels
 
 ggplot(data=df,aes(x=time, y=temperature))+geom_bar(stat="identity")+
-  geom_line()+xlim(c(6,23))+ylim(0,30)+ggtitle("13th of October 2020")+
-  xlab("Time (hours)") + ylab("Temperature (°C)")
+  geom_line()+xlim(c(6,23))+ylim(0,30)+ggtitle("18th of July 2021")+
+  xlab("Time (hours)") + ylab("Temperature (Â°C)")
 
 
 
@@ -132,7 +158,10 @@ ggplot(data=df,aes(x=time, y=temperature))+geom_bar(stat="identity")+
 
 ggplot(data=df,aes(x=time, y=temperature))+geom_point()+ geom_smooth(method="lm")
 
-#It seems there is no linear relation between time and temperature 
+#It seems there is no linear relation between time and temperature. What about a non-linear relation?
+
+
+ggplot(data=df,aes(x=time, y=temperature))+geom_point()+ geom_smooth(method="loess")
 
 
 
@@ -209,3 +238,10 @@ iris
 
 data(mtcars)
 mtcars
+
+
+
+
+#A reference for PCA, or google Principal Component Analysis R for further reading!
+
+# https://www.youtube.com/watch?v=_UVHneBUBW0
